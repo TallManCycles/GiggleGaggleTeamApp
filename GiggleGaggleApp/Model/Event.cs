d238cb96e78a11a7574d585c0bfeeef35346c307
TallManCycles <Aaronday@live.com.au>
2016-06-22 21:13:10 +1000

    Initial Commit

---
+++ GiggleGaggleApp/Model/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GiggleGaggleApp
+{
+	public class Event
+	{
+		public Event ()
+		{
+			
+		}
+
+		public string Title {
+			get;
+			set;
+		}
+		public string Description {
+			get;
+			set;
+		}
+		public DateTime Date {
+			get;
+			set;
+		}
+	}
+}
+
---
+++ GiggleGaggleApp/Pages/MasterPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GiggleGaggleApp
+{
+	public partial class MasterPage : ContentPage
+	{
+		public MasterPage ()
+		{
+			InitializeComponent ();
+
+			DateLabel.Text = DateTime.Now.ToString("d");
+		}
+
+		protected override void OnAppearing()
+		{
+			List<MenuItems> menu = new List<MenuItems> ();
+
+			MenuItems m = new MenuItems ();
+			m.MenuTitle = "Bikesquat Camera";
+
+			menu.Add (m);
+
+			MainList.ItemsSource = menu;
+		}
+	}
+
+	public class MenuItems
+	{
+		public MenuItems()
+		{
+			
+		}
+
+		public string MenuTitle {
+			get;
+			set;
+		}
+		public Type MenuType {
+			get;
+			set;
+		}
+	}
+}
+
